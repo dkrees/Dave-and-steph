221cce3ff1c4b4c6810aa27da1a22b1c83131d89
dkrees <dkrees@gmail.com>
2023-09-14 22:14:47 +0100

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function RootPage() {
   return (
-    <div className="flex min-h-screen flex-col bg-gradient-to-br from-sky-800 from-10% to-emerald-500 to-90%">
-      <div className="container mx-auto mt-24 flex max-w-sm flex-col gap-6 rounded-lg border-2 border-emerald-600 bg-white p-6 text-center font-light">
-        <h1 className="text-4xl ">Nothing to see here.</h1>
-        <p className="text-lg text-gray-700">
-          A blank canvas for designing and building high fidelity prototypes.
-        </p>
-        <p>
-          Edit the{" "}
-          <code className="rounded-md bg-gray-200 px-2 py-1">
-            src/app/page.tsx
-          </code>{" "}
-          to get started.
-        </p>
-        <p className="text-sm">
-          Or visit the{" "}
-          <a className="text-sky-600 underline" href="/cultivate">
-            Cultivate
-          </a>{" "}
-          page for more information and project pages.
-        </p>
+    <>
+      <div className="absolute bottom-0 left-0 right-0 top-0 z-0 h-screen w-full">
+        <Image
+          className="object-cover"
+          fill={true}
+          alt="background"
+          src="/images/savethedate-bg.jpg"
+        />
       </div>
-    </div>
+      <div className="container z-10 mx-auto mt-6 flex max-w-md flex-col gap-6 bg-white/10 p-6 text-left font-light text-neutral-700 ">
+        <div className="">
+          <h1 className="text-4xl font-light">Save the Date!</h1>
+          <h2 className="text-4xl font-medium md:text-6xl">
+            Saturday 8th June 2024
+          </h2>
+          <p className="my-4 text-lg">Wedding Ceremony and Party in Cardiff.</p>
+          <p>RSVP invitations and details to follow soon.</p>
+        </div>
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       // Extend colors with custom a color palette.
       // By default these colors will be made available everywhere in the framework like text color, border color and background color
       colors: {
-        'primary': '#2BB3BB',
-        'secondary': '#EF8547',
-        'accent': '#C41565',
-      }
+        primary: "#224014",
+        secondary: "#8C1F33",
+        accent: "#D9D6D2",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('autoprefixer'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("autoprefixer"),
   ],
-}
+};
